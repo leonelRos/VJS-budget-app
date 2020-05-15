5d1ef5d52212dcf4167b87ea43facfa578a98e59
Leonel Rosales <rleonel426@gmail.com>
2020-05-15 10:39:58 -0700

    adding modules

--- js/server.js
+++ js/server.js
@@ -1,2 +1,25 @@
-var x = " hello"
-console.log(x)
+var budgetController = (function () {
+
+})();
+
+let UIController = (function () {
+
+})();
+
+//GLOBAL APP CONTROLLER
+let controller = (function (budgetctlr, UICtrl) {
+    document.querySelector('.add__btn').addEventListener("click", function () {
+
+        //1. get the field input data
+
+        //2.Add item to the budget controller
+
+        //3.add item to UI
+
+        //4. Calculate the budget
+
+        //5.Display the budget on the UI
+
+    })
+
+})(budgetController, UIController);
