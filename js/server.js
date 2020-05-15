1592f469a1df024968c2f67a2f2aecf202a36b64
Leonel Rosales <rleonel426@gmail.com>
2020-05-15 11:49:59 -0700

    getting the value from input text

--- js/server.js
+++ js/server.js
@@ -3,14 +3,37 @@ var budgetController = (function () {
 })();
 
 let UIController = (function () {
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDesc: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        addingInput: function () {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDesc).value,
+                addValue: document.querySelector(DOMStrings.inputValue).value
+            }
+        },
+        getDOMstrings: function () {
+            return DOMStrings;
+        }
+    }
 
 })();
 
 //GLOBAL APP CONTROLLER
 let controller = (function (budgetctlr, UICtrl) {
-    document.querySelector('.add__btn').addEventListener("click", function () {
+    var DOM = UICtrl.getDOMstrings()
+
+    function addCtrlItem() {
 
         //1. get the field input data
+        var addedInput = UICtrl.addingInput()
+        console.log(addedInput)
 
         //2.Add item to the budget controller
 
@@ -20,6 +43,15 @@ let controller = (function (budgetctlr, UICtrl) {
 
         //5.Display the budget on the UI
 
+
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener("click", addCtrlItem)
+
+    document.addEventListener("keypress", function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            addCtrlItem()
+        }
     })
 
 })(budgetController, UIController);
